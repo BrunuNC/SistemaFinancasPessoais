0ed79918c9c276895b31e6df46b1856e109479ea
BrunuNC <brununc@gmail.com>
2021-12-03 17:43:48 -0400

    update

--- src/data/items.ts
+++ src/data/items.ts
@@ -4,5 +4,5 @@ export const items: Item[] = [
   { date: new Date(2021, 10, 6), category: 'food', title: 'McDonalds', value: 30.12 },
   { date: new Date(2021, 10, 15), category: 'food', title: 'Burger King', value: 28 },
   { date: new Date(2021, 10, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
-  { date: new Date(2021, 10, 18), category: 'salary', title: 'Salario Acme', value: 4300 }
+  { date: new Date(2021, 11, 18), category: 'salary', title: 'Salario Acme', value: 4300 }
 ];
